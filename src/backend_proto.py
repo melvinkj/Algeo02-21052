fix : change calculation of similarity and weight

src/backend_proto.py
@@ -102,8 +102,16 @@ def getEigenFaces(eigenSpace, A, k):
     best = eigenSpace[0:k]
     # print(np.array(best).shape)
     # print(np.array(A).shape)
+    # print("Ini best")
+    # print(np.array(best).shape)
+    # print(best)
+    # print("Ini A")
+    # print(np.array(A).shape)
+    # print(A)
     bestOriEigenFace = np.matmul(best, A)
-
+    # print(bestOriEigenFace)
+    # print("Ini best ori")
+    # print(bestOriEigenFace)
     return bestOriEigenFace
 
 # WEIGHT SET CALCULATOR
@@ -123,21 +131,84 @@ def getThreshold(eigenfaceWeight, M):
         start = i+1
         for j in range(start, M):
             if (i == 0 and j == 1):
-                leng = np.linalg.norm(eigenfaceWeight[j]) - np.linalg.norm(eigenfaceWeight[i])
+                # leng = np.linalg.norm(eigenfaceWeight[j]) - np.linalg.norm(eigenfaceWeight[i])
+                leng = (eigenfaceWeight[j]/np.linalg.norm(eigenfaceWeight[j])) - (eigenfaceWeight[i]/np.linalg.norm(eigenfaceWeight[i]))
                 max = np.dot(np.transpose(leng), leng)
             else:
-                curr = np.linalg.norm(eigenfaceWeight[j]) - np.linalg.norm(eigenfaceWeight[i])
+                curr = (eigenfaceWeight[j]/np.linalg.norm(eigenfaceWeight[j])) - (eigenfaceWeight[i]/np.linalg.norm(eigenfaceWeight[i]))
+                # curr = eigenfaceWeight[j] - eigenfaceWeight[i]
                 distance = np.dot(np.transpose(curr), curr)
                 if (max < distance):
                     max = distance
 
     t = 0.5*max
+    print("Ini threshold")
     print(t)
     return t
 
 # MATCHER
 
 
+# def matcher(input, datasetName, mean, weightSet, M, threshold, eigenfaces):
+#     folder = [os.path.join(datasetName, p)
+#               for p in sorted(os.listdir(datasetName))]
+#     # perlu transpose input dan training set dulu
+#     # print(np.array(mean).shape)
+#     # print(mean)
+#     selisih = A([extract_features(input)], mean)
+#     # print(np.array(selisih).shape)
+#     weight = [0 for i in range(M)]
+
+#     weight = np.matmul(selisih[0], np.transpose(eigenfaces))
+#     # print(weight)
+#     # print(np.array(weight).shape)
+
+#     for i in range(M):
+#         # Normalisasi vektor
+#         # norm_i = np.linalg.norm(weightSet[i])
+#         # norm = np.linalg.norm(weight)
+#         # vecNorm_i = np.multiply(weightSet[i], 1/norm_i)
+#         # vecNorm = np.multiply(weight, 1/norm)
+
+#         # Formula Euclidean Distance 
+#         subResult = np.subtract(weightSet[i], weight)
+#         normResult = np.linalg.norm(subResult)
+#         distance = (normResult**2)/2
+
+#         # Rumus cos(teta) = 1 - ||x-x'||^2 / 2 = cos (teta)
+#         # nilai distance adalah ||x-x'|| ^2 /2 , boleh lebih dari 1. Namun bila lebih dari satu maka dikurangi 1 (mendekati cos 0)
+#         if (distance > 1):
+#             distance -=1
+
+#         print("index", i+1, ": ", distance)
+#         if (i == 0):
+#             min = distance
+#             # min = np.linalg.norm(np.subtract(weight, weightSet[i]))
+#             # print("distance ", i, ": ")
+#             # print(min)
+#             index = i
+#             print("Ini distance ", str(i), " : ", str(min))
+
+#         else:
+#             leng = np.linalg.norm(weightSet[i]) - np.linalg.norm(weight)
+#             distance = np.dot(np.transpose(leng), leng)
+#             print("Ini distance ", str(i), " : ", str(distance))
+#             # distance = np.linalg.norm(np.subtract(weight, weightSet[i]))
+#             # print("distance ", i, ":")
+#             # print(distance)
+#             if (distance < min):
+#                 min = distance
+#                 index = i
+
+#     if (min > threshold):
+#         # Kasus tidak ada yang mirip
+#         match = False
+#     else:
+#         match = True
+#         matchedPath = folder[index]
+
+
+#     return match, matchedPath, min
 def matcher(input, datasetName, mean, weightSet, M, threshold, eigenfaces):
     folder = [os.path.join(datasetName, p)
               for p in sorted(os.listdir(datasetName))]
@@ -145,60 +216,54 @@ def matcher(input, datasetName, mean, weightSet, M, threshold, eigenfaces):
     # print(np.array(mean).shape)
     # print(mean)
     selisih = A([extract_features(input)], mean)
+    # print("Ini selisih:")
+    # print(selisih)
     # print(np.array(selisih).shape)
     weight = [0 for i in range(M)]
-
+    # print("Ini eigenfaces:")
+    # print(eigenfaces)
+    
     weight = np.matmul(selisih[0], np.transpose(eigenfaces))
+    # print("Ini weight: ")
+    # print(weight)
     # print(weight)
     # print(np.array(weight).shape)
 
     for i in range(M):
-        # Normalisasi vektor
-        norm_i = np.linalg.norm(weightSet[i])
-        norm = np.linalg.norm(weight)
-        vecNorm_i = np.multiply(weightSet[i], 1/norm_i)
-        vecNorm = np.multiply(weight, 1/norm)
-
-        # Formula Euclidean Distance 
-        subResult = np.subtract(vecNorm_i, vecNorm)
-        normResult = np.linalg.norm(subResult)
-        distance = (normResult**2)/2
-
-        # Rumus cos(teta) = 1 - ||x-x'||^2 / 2 = cos (teta)
-        # nilai distance adalah ||x-x'|| ^2 /2 , boleh lebih dari 1. Namun bila lebih dari satu maka dikurangi 1 (mendekati cos 0)
-        if (distance > 1):
-            distance -=1
-
-        print("index", i+1, ": ", distance)
         if (i == 0):
-            min = distance
+            # leng = np.linalg.norm(weightSet[i]) - np.linalg.norm(weight)
+            leng = (weightSet[i]/np.linalg.norm(weightSet[i])) - (weight/np.linalg.norm(weight))
+            min = np.dot(np.transpose(leng), leng)
             # min = np.linalg.norm(np.subtract(weight, weightSet[i]))
             # print("distance ", i, ": ")
             # print(min)
             index = i
-            print("Ini distance ", str(i), " : ", str(min))
-
+            distanceWeight = leng
         else:
-            leng = np.linalg.norm(weightSet[i]) - np.linalg.norm(weight)
+            # leng = np.linalg.norm(weightSet[i]) - np.linalg.norm(weight)
+            leng = (weightSet[i]/np.linalg.norm(weightSet[i])) - (weight/np.linalg.norm(weight))
             distance = np.dot(np.transpose(leng), leng)
-            print("Ini distance ", str(i), " : ", str(distance))
+            # print("Ini min")
+            # print(min)
+            # print("ini distance")
+            # print(distance)
             # distance = np.linalg.norm(np.subtract(weight, weightSet[i]))
             # print("distance ", i, ":")
             # print(distance)
             if (distance < min):
                 min = distance
                 index = i
+                distanceWeight = leng
 
     if (min > threshold):
-        # Kasus tidak ada yang mirip
         match = False
     else:
         match = True
         matchedPath = folder[index]
 
-
-    return match, matchedPath, min
-
+    print("Ini distance")
+    print(min)
+    return match, matchedPath, min, distanceWeight, weight
 
 # QR DECOMPOSITION
 
@@ -296,8 +361,8 @@ def find_eig(matrix):
 def sorted_eig(matrix):
     eigVal, eigVec = find_eig(matrix)
     eigVec = np.transpose(eigVec)
-    for i in range (len(eigVec)):
-        eigVec[i] = np.linalg.norm(eigVec[i])
+    # for i in range (len(eigVec)):
+    #     eigVec[i] = np.linalg.norm(eigVec[i])
 
     # Sorting from largest to smallest eigVal
     sortedEigVal = np.sort(eigVal)[::-1]

src/main.py
@@ -178,23 +178,26 @@ def MainPage():
         # print("lib")
         # print(eiglib)
         # print(eigvlib)
-        k = int(M)
+        k = int(M*0.7)
         eigenFaces = getEigenFaces(eigenSpace, a, k)
-        print("1 ")
+        # print("1 ")
         weightSet = getWeightSet(a, eigenFaces, M)
-        print(weightSet)
+        # print(weightSet)
         # print("2 ")
         # weightSet = weightSetC(a, eigenFaces, M)
         # print(weightSet)
         threshold = getThreshold(weightSet, M)
         match = True
 
-        match, matchedPath, distance = matcher(userImageName, dataSetName, average, weightSet, M,
+        match, matchedPath, distance, distanceWeight, weight = matcher(userImageName, dataSetName, average, weightSet, M,
                                      threshold, eigenFaces)
   
         # Cari similarity
         # cos (teta) = 1 - distance , semakin besar maka semakin mirip karena teta mendekati 0
-        similarity = 1 - distance
+        # distanceWeight = np.linalg.norm(distanceWeight)
+        # weight = np.linalg.norm(weight)
+
+        similarity = 1 /(1+distance)
         percentage = (int (similarity*10000)) /100 # membuat dia hanya 2 angka dibelakang koma
         percentageLabel.config(text="( "+str(percentage)+"% )")
 